7ce82afd03ef61b14c03d152fb56d9e2749c2a92
Brett Kaplan <BrettCKaplan@gmail.com>
2013-03-23 02:58:59 -0400

    More/better commenting everywhere.

--- src/game.py
+++ src/game.py
@@ -1,19 +1,22 @@
-#game stuff
+#This is the game file.
 import pygame
-#I forget if this needs to inherit from object, I think it's a pygame thing?
 class Game(object):
     def __init__(self):
+        """Start and create things as needed."""
         pygame.init()
         self.running = 1
         self.screen = pygame.display.set_mode((128, 128))
         
     def __del__(self):
+        """End and delete things as needed."""
         pygame.quit()
         
     def run(self):
+        """Run the game, and check if the game needs to end."""
         return self.running and self.input() and self.update() and self.draw()
         
     def input(self):
+        """Take inputs as needed."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 0
@@ -21,13 +24,14 @@ class Game(object):
                 if event.key == pygame.K_ESCAPE:
                     return 0
                 if event.key == pygame.K_q:
-                    self.running = 0#setting running to 0 will end the next run
+                    self.running = 0
         return 1
         
     def update(self):
+        """Update things as needed."""
         return 1
         
     def draw(self):
-        #draw stuff
+        """Draw things as needed."""
         pygame.display.flip()
         return 1
--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
-#main python file, runs game, handles arguments if we want, etc.
+#This is the main python file.
+#If we want to take in arguments, we would use this file.
+#This is where we create and run the game.
 import sys
 from game import *
 
 g = Game()
 while g.run():
     #stuff can be put here, in which case pass is not needed
-    pass#maybe this should be continue, I don't remember
+    pass
     
 del g
 sys.exit()

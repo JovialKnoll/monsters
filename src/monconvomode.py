b602b9b59140bbfd07da32097f1a40626f666957
Brett Kaplan <BrettCKaplan@gmail.com>
2017-10-15 17:22:08 -0400

    realized that MonConvoMode needs to be sure to draw the right image

--- src/monconvomode.py
+++ src/monconvomode.py
@@ -3,6 +3,6 @@ import sharedstate
 from convomode import *
 
 class MonConvoMode(ConvoMode):
-    def __init__(self):
-        super(MonConvoMode, self).__init__()
-        sharedstate.state.protag_mon.drawCentered(self.background, (160,128))
+    def _drawScreen(self, screen):
+        super(MonConvoMode, self)._drawScreen(screen)
+        sharedstate.state.protag_mon.drawCentered(screen, (160,128))

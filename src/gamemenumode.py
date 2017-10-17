7509c32850673b06341804988a7a3714fc1b9d13
Brett Kaplan <BrettCKaplan@gmail.com>
2017-10-17 17:47:23 +0000

    no magic numbers

--- src/gamemenumode.py
+++ src/gamemenumode.py
@@ -10,6 +10,8 @@ from gamemode import GameMode
 class GameMenuMode(GameMode):
     class State(object):
         Menu, Save, Load = range(3)
+    menu_width = 20
+    file_extension = '.sav'
     def __init__(self, previous_mode):
         super(GameMenuMode, self).__init__()
         self.state = GameMenuMode.State.Menu
@@ -31,7 +33,13 @@ class GameMenuMode(GameMode):
         objects = ['asd', (1,2,3), 123]
         if not os.path.exists(constants.SAVE_DIRECTORY):
             os.makedirs(constants.SAVE_DIRECTORY)
-        with open(os.path.join(constants.SAVE_DIRECTORY, self.save_name + '.sav'), 'wb') as f:
+        with open(
+            os.path.join(
+                constants.SAVE_DIRECTORY,
+                self.save_name + self.__class__.file_extension
+            ),
+            'wb'
+        ) as f:
             pickle.dump(objects, f, pickle.HIGHEST_PROTOCOL)
 
     def _inputMenu(self, event):
@@ -63,7 +71,7 @@ class GameMenuMode(GameMode):
                     self._saveGame()
                     self.state = GameMenuMode.State.Menu
             elif event.key == pygame.K_LEFT:
-                self.cursor_position = max(self.cursor_position-1, 0)
+                self.cursor_position = max(self.cursor_position - 1, 0)
                 self._resetCursorBlink()
             elif event.key == pygame.K_RIGHT:
                 self.cursor_position = min(self.cursor_position+1, length)
@@ -82,7 +90,15 @@ class GameMenuMode(GameMode):
                     self.save_name = self.save_name[:self.cursor_position-1] + self.save_name[self.cursor_position:]
                     self.cursor_position -= 1
                 self._resetCursorBlink()
-            elif length < 16 and ((char >= '0' and char <= '9' ) or (event.key > 96 and event.key < 123)):# numbers and letters
+            elif (
+                length < (self.__class__.menu_width - len(self.__class__.file_extension))
+                and (
+                    # numbers
+                    (char >= '0' and char <= '9' )
+                    # or letters
+                    or (event.key > 96 and event.key < 123)
+                )
+            ):
                 self.save_name = self.save_name[:self.cursor_position] + char + self.save_name[self.cursor_position:]
                 self.cursor_position += 1
                 self._resetCursorBlink()
@@ -122,23 +138,47 @@ class GameMenuMode(GameMode):
         screen.blit(self.old_screen, (0,0))
         if self.state is GameMenuMode.State.Menu:
             disp_text = "Options:\n_Go Back (ESC)\n_Save (F1)\n_Load (F2)\n_Quit (F3)"
-            shared.font_wrap.renderToInside(screen, (0,0), 20 * constants.FONT_SIZE, disp_text, False, constants.WHITE, constants.BLACK)
+            shared.font_wrap.renderToInside(
+                screen,
+                (0,0),
+                self.__class__.menu_width * constants.FONT_SIZE,
+                disp_text,
+                False,
+                constants.WHITE,
+                constants.BLACK
+            )
             # center this, make bigger and buttons... maybe
             # more to come
         elif self.state is GameMenuMode.State.Save:
             disp_text = "Options:\n_Go Back (ESC)\n_Save (ENTER)\nType a file name:\n"
             if self.save_name:
                 disp_text += self.save_name
-            disp_text += ".sav"
-            shared.font_wrap.renderToInside(screen, (0,0), 20 * constants.FONT_SIZE, disp_text, False, constants.WHITE, constants.BLACK)
+            disp_text += self.__class__.file_extension
+            shared.font_wrap.renderToInside(
+                screen,
+                (0,0),
+                self.__class__.menu_width * constants.FONT_SIZE,
+                disp_text,
+                False,
+                constants.WHITE,
+                constants.BLACK
+            )
             if self.cursor_switch:
                 screen.fill(constants.WHITE, ((self.cursor_position * constants.FONT_SIZE, 40), (1, 10)))
             # display prompt for file to save
             # display save_name in there
         elif self.state is GameMenuMode.State.Load:
             disp_text = "Options:\n_Go Back (ESC)\n_Load (ENTER)\nSelect a file name:\n"
-            disp_text += ".sav"
-            shared.font_wrap.renderToInside(screen, (0,0), 20 * constants.FONT_SIZE, disp_text, False, constants.WHITE, constants.BLACK)
+            disp_text += self.__class__.file_extension
+            shared.font_wrap.renderToInside(
+                screen,
+                (0,0),
+                self.__class__.menu_width * constants.FONT_SIZE,
+                disp_text,
+                False,
+                constants.WHITE,
+                constants.BLACK
+            )
             # draw load thingy
             pass
         else:

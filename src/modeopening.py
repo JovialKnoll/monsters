c6316d2610b4b119af92236b28b7519125a9adb2
Brett Kaplan <BrettCKaplan@gmail.com>
2017-11-04 15:12:20 -0400

    more variables for fun and profit

--- src/modeopening.py
+++ src/modeopening.py
@@ -18,51 +18,55 @@ class ModeOpening(Mode):
         super(ModeOpening, self).__init__()
         self.left_mon = Monster.atLevel(3)
         self.right_mon = Monster.atLevel(2)
+        self.all_sprites.add(self.right_mon, self.left_mon)
         # starts at right
         self.left_mon.rect.bottomright = constants.SCREEN_SIZE
         self.left_mon.setImage(True)
         # starts at left
         self.right_mon.rect.bottomleft = (0, constants.SCREEN_SIZE[1])
         # move to the positions
-        self.left_mon.addPosRel(Monster.Lerp, 250 * 6, -constants.SCREEN_SIZE[0] // 2, 0)
-        self.right_mon.addPosRel(Monster.Lerp, 250 * 6, constants.SCREEN_SIZE[0] // 2, 0)
+        beat = 250
+        pause = 50
+        self.left_mon.addPosRel(Monster.Lerp, beat * 6, -constants.SCREEN_SIZE[0] // 2, 0)
+        self.right_mon.addPosRel(Monster.Lerp, beat * 6, constants.SCREEN_SIZE[0] // 2, 0)
         # back and forth
-        self.left_mon.addWait(250 * 8 + 50 * 2)
+        self.left_mon.addWait(beat * 8 + pause * 2)
         jump = self.right_mon.rect.width // 8
-        self.right_mon.addPosRel(Monster.Lerp, 250, jump, -jump)
-        self.right_mon.addPosRel(Monster.Lerp, 250, jump, jump)
-        self.right_mon.addPosRel(Monster.Lerp, 250, -jump, -jump)
-        self.right_mon.addPosRel(Monster.Lerp, 250, -jump, jump)
-        self.right_mon.addWait(50)
-        self.right_mon.addPosRel(Monster.Lerp, 250, jump, -jump)
-        self.right_mon.addPosRel(Monster.Lerp, 250, jump, jump)
-        self.right_mon.addPosRel(Monster.Lerp, 250, -jump, -jump)
-        self.right_mon.addPosRel(Monster.Lerp, 250, -jump, jump)
-        self.right_mon.addWait(50)
+        self.right_mon.addPosRel(Monster.Lerp, beat, jump, -jump)
+        self.right_mon.addPosRel(Monster.Lerp, beat, jump, jump)
+        self.right_mon.addPosRel(Monster.Lerp, beat, -jump, -jump)
+        self.right_mon.addPosRel(Monster.Lerp, beat, -jump, jump)
+        self.right_mon.addWait(pause)
+        self.right_mon.addPosRel(Monster.Lerp, beat, jump, -jump)
+        self.right_mon.addPosRel(Monster.Lerp, beat, jump, jump)
+        self.right_mon.addPosRel(Monster.Lerp, beat, -jump, -jump)
+        self.right_mon.addPosRel(Monster.Lerp, beat, -jump, jump)
+        self.right_mon.addWait(pause)
         # small pause
-        self.left_mon.addWait(250)
-        self.right_mon.addWait(250)
+        self.left_mon.addWait(beat)
+        self.right_mon.addWait(beat)
         # slash!
         self.left_mon.addPosRel(Monster.Lerp, 100, -jump // 2, -jump // 3)
         self.left_mon.addPosRel(Monster.Lerp, 200, jump + jump // 2, jump // 3)
         self.right_mon.addWait(300)
         # jump back
-        self.left_mon.addWait(250)
-        self.right_mon.addPosRel(Monster.Lerp, 125, jump, -jump * 2)
-        self.right_mon.addPosRel(Monster.Lerp, 125, jump, jump * 2)
+        self.left_mon.addWait(beat)
+        self.right_mon.addPosRel(Monster.Lerp, beat // 2, jump * 2, -jump * 2)
+        self.right_mon.addPosRel(Monster.Lerp, beat // 2, jump * 2, jump * 2)
         # pause
         self.left_mon.addWait(150)
         self.left_mon.addPosRel(Monster.Lerp, 100, -jump, 0)
-        self.right_mon.addWait(250)
+        self.right_mon.addWait(beat)
         # back and forth again
-        self.left_mon.addWait(250 * 4)
-        self.right_mon.addPosRel(Monster.Lerp, 250, -jump, -jump * 2)
-        self.right_mon.addPosRel(Monster.Lerp, 250, -jump, jump * 2)
-        self.right_mon.addPosRel(Monster.Lerp, 250, jump, -jump * 2)
-        self.right_mon.addPosRel(Monster.Lerp, 250, jump, jump * 2)
-        # add to all_sprites
-        self.all_sprites.add(self.right_mon, self.left_mon)
-
+        self.left_mon.addWait(beat * 4)
+        self.right_mon.addPosRel(Monster.Lerp, beat, -jump, -jump * 2)
+        self.right_mon.addPosRel(Monster.Lerp, beat, -jump, jump * 2)
+        self.right_mon.addPosRel(Monster.Lerp, beat, jump, -jump * 2)
+        self.right_mon.addPosRel(Monster.Lerp, beat, jump, jump * 2)
+        # charge
+        # self.left_mon.addWait(beat)
+        # self.right_mon.addPosRel(Monster.Lerp, beat, -jump, -jump * 2)
+        # fire
     def _changeMode(self):
         self.next_mode = ModeConvo0()
 

f3f1d7a54ed1c2268fc4f918ce15cf6159de8141
JovialKnoll <BrettCKaplan@gmail.com>
2021-04-10 17:13:45 -0400

    adding the star thingy and more color changes

--- src/constants.py
+++ src/constants.py
@@ -6,6 +6,7 @@ CURSOR_TIME = 500
 FONT_SIZE = 8
 FONT_HEIGHT = 10
 TEXT_COLOR = (164, 162, 165)
+DARK_TEXT_COLOR = (82, 81, 83)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 COLORKEY = (255, 0, 255)
@@ -14,6 +15,8 @@ GRAPHICS_DIRECTORY = 'gfx'
 FONT_FILE = os.path.join(GRAPHICS_DIRECTORY, 'simple_mono.ttf')
 JK_LOGO_BLACK = os.path.join(GRAPHICS_DIRECTORY, 'jklogo_black.png')
 JK_LOGO_GREY = os.path.join(GRAPHICS_DIRECTORY, 'jklogo_grey.png')
+JK_LOGO_LIGHT_GREY = os.path.join(GRAPHICS_DIRECTORY, 'jklogo_light_grey.png')
+STAR = os.path.join(GRAPHICS_DIRECTORY, 'star.png')
 
 BACKGROUNDS_DIRECTORY = os.path.join(GRAPHICS_DIRECTORY, 'backgrounds')
 BLACKBOX_FILE = os.path.join(BACKGROUNDS_DIRECTORY, 'blackbox.png')
--- src/modeopening0.py
+++ src/modeopening0.py
@@ -4,6 +4,7 @@ import constants
 import shared
 from mode import Mode
 from modeopening1 import ModeOpening1
+from animsprite import AnimSprite
 
 class ModeOpening0(Mode):
     __slots__ = (
@@ -36,6 +37,22 @@ class ModeOpening0(Mode):
                 constants.SCREEN_SIZE[1] * 7 // 16 - jk_logo.get_height() // 2,
             )
         )
+        star_sprite = AnimSprite()
+        star_sprite.image = pygame.image.load(constants.STAR).convert(shared.display.screen)
+        star_sprite.image.set_colorkey(constants.COLORKEY)
+        star_sprite.rect = star_sprite.image.get_rect()
+        star_sprite.rect.center = (
+            constants.SCREEN_SIZE[0] // 2 + constants.SCREEN_SIZE[1] // 2 + star_sprite.rect.width // 2,
+            star_sprite.rect.height // 2 * -1,
+        )
+        star_sprite.addWait(750)
+        star_sprite.addPosAbs(
+            AnimSprite.Lerp,
+            500,
+            constants.SCREEN_SIZE[0] // 2 - constants.SCREEN_SIZE[1] // 2 - star_sprite.rect.width // 2,
+            constants.SCREEN_SIZE[1] + star_sprite.rect.height // 2
+        )
+        self.all_sprites.add(star_sprite)
 
     def _changeMode(self):
         self.next_mode = ModeOpening1()
@@ -46,7 +63,8 @@ class ModeOpening0(Mode):
 
     def _update(self, dt):
         self.time += dt
-        if self.time >= 1000 and self.step < 1:
+        if self.time >= 1250 and self.step < 1:
+            self.step += 1
             shared.font_wrap.renderToCentered(
                 self.background,
                 (constants.SCREEN_SIZE[0] // 2, constants.SCREEN_SIZE[1] * 5 // 8),
@@ -55,6 +73,24 @@ class ModeOpening0(Mode):
                 constants.TEXT_COLOR
             )
         if self.time >= 1500 and self.step < 2:
+            self.step += 1
+            shared.font_wrap.renderToCentered(
+                self.background,
+                (constants.SCREEN_SIZE[0] // 2, constants.SCREEN_SIZE[1] * 5 // 8),
+                self.__class__.logo_text,
+                False,
+                constants.DARK_TEXT_COLOR
+            )
+            jk_logo = pygame.image.load(constants.JK_LOGO_LIGHT_GREY).convert(shared.display.screen)
+            self.background.blit(
+                jk_logo,
+                (
+                    constants.SCREEN_SIZE[0] // 2 - jk_logo.get_width() // 2,
+                    constants.SCREEN_SIZE[1] * 7 // 16 - jk_logo.get_height() // 2,
+                )
+            )
+        if self.time >= 1750 and self.step < 3:
+            self.step += 1
             shared.font_wrap.renderToCentered(
                 self.background,
                 (constants.SCREEN_SIZE[0] // 2, constants.SCREEN_SIZE[1] * 5 // 8),
@@ -70,7 +106,7 @@ class ModeOpening0(Mode):
                     constants.SCREEN_SIZE[1] * 7 // 16 - jk_logo.get_height() // 2,
                 )
             )
-        if self.time >= 3000 and self.step < 4:
+        if self.time >= 3500 and self.step < 4:
             self._changeMode()
             pass
 

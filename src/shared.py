cf9a9b0fe0329f35969fb1406206b7d100ddf21c
JovialKnoll <BrettCKaplan@gmail.com>
2021-05-02 16:42:49 -0400

    don't need to use random.seed

--- src/shared.py
+++ src/shared.py
@@ -1,11 +1,8 @@
-import random
-
 import constants
 from display import Display
 from fontwrap import FontWrap
 from state import State
 
-random.seed()
 game_running = True
 display = Display()
 font_wrap = FontWrap(constants.FONT, constants.FONT_SIZE)

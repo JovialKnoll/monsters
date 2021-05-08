0e4047ad61ad8052858b1b7a558973e5a97b769b
JovialKnoll <BrettCKaplan@gmail.com>
2021-05-08 16:47:50 -0400

    reorganizing pygame setup

--- src/main.py
+++ src/main.py
@@ -8,15 +8,14 @@ from game import Game
 
 
 def main():
-    pygame.init()
     game = Game()
     while game.run():
         pass
     del game
-    pygame.quit()
 
 
+pygame.init()
 if __name__ == "__main__":
     main()
-
+pygame.quit()
 sys.exit()

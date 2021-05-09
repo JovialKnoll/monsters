37915392371747083abe571853db784d8a458379
JovialKnoll <BrettCKaplan@gmail.com>
2021-05-08 23:40:27 -0400

    updating config

--- src/display.py
+++ src/display.py
@@ -46,9 +46,13 @@ class Display(object):
         self._is_fullscreen = shared.config.getboolean(constants.CONFIG_SECTION, constants.CONFIG_FULLSCREEN)
         self._upscale = shared.config.getint(constants.CONFIG_SECTION, constants.CONFIG_SCREEN_SCALE)
         self._upscale = max(min(self._upscale, self._upscale_max), 0)
+        shared.config.set(constants.CONFIG_SECTION, constants.CONFIG_SCREEN_SCALE, self._upscale)
         self._full_screen = pygame.Surface(self._disp_res_max)
         self.screen = pygame.Surface(constants.SCREEN_SIZE)
-        self._scaleWindow()
+        if self._is_fullscreen:
+            self._setFullscreen()
+        else:
+            self._scaleWindow()
 
     def _scaleWindow(self, scale_change=0):
         """Set the window to a new scale."""
@@ -56,6 +60,7 @@ class Display(object):
         if new_upscale < 1 or new_upscale > self._upscale_max:
             return
         self._upscale = new_upscale
+        shared.config.set(constants.CONFIG_SECTION, constants.CONFIG_SCREEN_SCALE, self._upscale)
         self._disp_res = (
             constants.SCREEN_SIZE[0] * self._upscale,
             constants.SCREEN_SIZE[1] * self._upscale,
@@ -78,10 +83,7 @@ class Display(object):
         if not self._is_fullscreen:
             self._scaleWindow(scale_change)
 
-    def toggleFullscreen(self):
-        if self._is_fullscreen:
-            self._scaleWindow()
-            return
+    def _setFullscreen(self):
         pygame.display.quit()
         pygame.display.init()
         self._disp_screen = pygame.display.set_mode(
@@ -92,6 +94,13 @@ class Display(object):
         self._full_screen = self._full_screen.convert(self._disp_screen)
         self.screen = self.screen.convert(self._full_screen)
         self._is_fullscreen = True
+
+    def toggleFullscreen(self):
+        if self._is_fullscreen:
+            self._scaleWindow()
+        else:
+            self._setFullscreen()
+        shared.config.set(constants.CONFIG_SECTION, constants.CONFIG_FULLSCREEN, self._is_fullscreen)
 
     def scaleMouseInput(self, event):
         """Scale mouse position for events in terms of the screen (as opposed to the display surface)."""

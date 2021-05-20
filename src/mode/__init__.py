4ecb683e5f7782b0d9dd3d7ca005a1075c3c7f6b
JovialKnoll <BrettCKaplan@gmail.com>
2021-05-19 20:38:52 -0400

    adding new mode and alphabetical

--- src/mode/__init__.py
+++ src/mode/__init__.py
@@ -1,21 +1,17 @@
 from .mode import Mode
-
+from .modeconvo import ModeConvo
+from .modefight import ModeFight
 from .modegamemenu import ModeGameMenu
 from .modegamemenu import ModeGameMenuTop
 from .modegamemenu import ModeGameMenuSave
 from .modegamemenu import ModeGameMenuLoad
 from .modegamemenu import ModeGameMenuOptions
-
+from .modeintroduction0 import ModeIntroduction0
+from .modemenu import ModeMenu
+from .modemonconvo import ModeMonConvo
+from .modemonconvo0 import ModeMonConvo0
 from .modeopening0 import ModeOpening0
 from .modeopening1 import ModeOpening1
 from .modeopening2 import ModeOpening2
 from .modeopening3 import ModeOpening3
-
-from .modefight import ModeFight
-
-from .modeconvo import ModeConvo
-from .modemonconvo import ModeMonConvo
-from .modemenu import ModeMenu
-from .modemonconvo0 import ModeMonConvo0
-
 from .modetest import ModeTest

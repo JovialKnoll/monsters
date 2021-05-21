75988fef88d1d8d4c05cf9b636fbaa56cb1757be
JovialKnoll <BrettCKaplan@gmail.com>
2021-05-21 18:42:06 -0400

    make variables protected

--- src/mode/modeconvo.py
+++ src/mode/modeconvo.py
@@ -31,21 +31,21 @@ class ModeConvo(Mode, Saveable, abc.ABC):
     black_box.set_colorkey(constants.COLORKEY)
 
     __slots__ = (
-        'read_text',
-        'background',
-        'text_rect',
-        'text_scroll',
-        'surf_text',
-        'convo_key',
+        '_read_text',
+        '_background',
+        '_text_rect',
+        '_text_scroll',
+        '_surf_text',
+        '_convo_key',
     )
 
     def __init__(self, convo_key=None):
         super().__init__()
-        self.convo_key = convo_key
+        self._convo_key = convo_key
         self._renderText()
 
     def save(self):
-        return self.convo_key
+        return self._convo_key
 
     @classmethod
     def load(cls, save_data):
@@ -53,15 +53,15 @@ class ModeConvo(Mode, Saveable, abc.ABC):
         return new_obj
 
     def _renderText(self):
-        self.read_text = False
-        self.text_rect = pygame.Rect(0, 0, 288, 48)
-        self.text_scroll = 0
-        self.surf_text = shared.font_wrap.renderInside(288, self._textMain(), False, constants.TEXT_COLOR)
-        self.background = pygame.image.load(constants.LAYOUT_1_FILE).convert(shared.display.screen)
-        self.background.set_colorkey(constants.COLORKEY)
+        self._read_text = False
+        self._text_rect = pygame.Rect(0, 0, 288, 48)
+        self._text_scroll = 0
+        self._surf_text = shared.font_wrap.renderInside(288, self._textMain(), False, constants.TEXT_COLOR)
+        self._background = pygame.image.load(constants.LAYOUT_1_FILE).convert(shared.display.screen)
+        self._background.set_colorkey(constants.COLORKEY)
         for index, rect in enumerate(type(self).boxes.rects):
             shared.font_wrap.renderToInside(
-                self.background,
+                self._background,
                 self.boxes.textStart(index),
                 self.boxes.textWidth(index),
                 self._textButton(index),
@@ -89,17 +89,17 @@ class ModeConvo(Mode, Saveable, abc.ABC):
             self.boxes.posSelect(event.pos)
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
-                if self.read_text \
+                if self._read_text \
                     and self.boxes.posSelect(event.pos) is not None \
                     and self._mouseButtonStatus(event.button) \
                     and self.boxes.posSelect(self._mouseButtonStatus(event.button)) \
                         == self.boxes.posSelect(event.pos):
                     self._goButton(self.boxes.select)
             elif event.button == 4:
-                self.text_rect.move_ip(0, -constants.FONT_HEIGHT)
+                self._text_rect.move_ip(0, -constants.FONT_HEIGHT)
             elif event.button == 5:
-                self.text_rect.move_ip(0, constants.FONT_HEIGHT)
-        elif event.type == pygame.KEYDOWN and self.read_text:
+                self._text_rect.move_ip(0, constants.FONT_HEIGHT)
+        elif event.type == pygame.KEYDOWN and self._read_text:
             if event.key == pygame.K_RETURN:
                 self._goButton(self.boxes.select)
             else:
@@ -107,19 +107,19 @@ class ModeConvo(Mode, Saveable, abc.ABC):
 
     def update(self, dt):
         pressed_keys = pygame.key.get_pressed()
-        self.text_scroll, text_scroll_int = utility.getIntMovement(
-            self.text_scroll,
+        self._text_scroll, text_scroll_int = utility.getIntMovement(
+            self._text_scroll,
             (pressed_keys[pygame.K_DOWN] - pressed_keys[pygame.K_UP]) * self.SCROLL_AMOUNT_SPEED,
             dt
         )
-        self.text_rect.move_ip(0, text_scroll_int)
-        self.text_rect.clamp_ip(self.surf_text.get_rect())
-        if self.text_rect.bottom >= self.surf_text.get_rect().bottom:
-            self.read_text = True
+        self._text_rect.move_ip(0, text_scroll_int)
+        self._text_rect.clamp_ip(self._surf_text.get_rect())
+        if self._text_rect.bottom >= self._surf_text.get_rect().bottom:
+            self._read_text = True
 
     def _drawScreen(self, screen):
         screen.fill(constants.WHITE)
-        screen.blit(self.background, (0, 0))
-        screen.blit(self.surf_text, (16, 16), self.text_rect)
-        if self.read_text:
+        screen.blit(self._background, (0, 0))
+        screen.blit(self._surf_text, (16, 16), self._text_rect)
+        if self._read_text:
             screen.blit(self.black_box, self.boxes.getSelectRect())

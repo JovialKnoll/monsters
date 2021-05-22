6d2300ed4f5bc7c6ede0017b6f6b2bbc5e94815a
JovialKnoll <BrettCKaplan@gmail.com>
2021-05-22 13:48:59 -0400

    taking a stab at writing more

--- src/mode/modeintroduction0.py
+++ src/mode/modeintroduction0.py
@@ -6,5 +6,5 @@ from .modeconvo import ModeConvo
 
 class ModeIntroduction0(ModeConvo):
     def _handleButton(self, prev_convo_key: str, index: int):
-        if prev_convo_key == '2' and index == 0:
+        if prev_convo_key == '6':
             shared.state.protag_mon = Monster()

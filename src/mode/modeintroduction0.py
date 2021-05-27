8c0ad6f4df3bb171b50df6d498ed01be794fd270
JovialKnoll <BrettCKaplan@gmail.com>
2021-05-26 21:33:56 -0400

    just making monster at start

--- src/mode/modeintroduction0.py
+++ src/mode/modeintroduction0.py
@@ -5,6 +5,4 @@ from .modeconvo import ModeConvo
 
 
 class ModeIntroduction0(ModeConvo):
-    def _handleButton(self, prev_convo_key: str, index: int):
-        if prev_convo_key == '6':
-            shared.state.protag_mon = Monster()
+    pass

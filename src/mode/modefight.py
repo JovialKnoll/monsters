e53353511b97f8d6d65f18ef4a299fd2cbe3516b
JovialKnoll <BrettCKaplan@gmail.com>
2021-05-31 12:40:02 -0400

    make fight properties protected

--- src/mode/modefight.py
+++ src/mode/modefight.py
@@ -44,115 +44,115 @@ class ModeFight(ModeButtons):
     ]
 
     __slots__ = (
-        'background',
-        'thunk',
-        'rooeee',
-        'bwop',
-        'player_mon',
-        'enemy_mon',
-        'player_action',
-        'enemy_action',
-        'action_display',
-        'action_set',
-        'result_displayed',
-        'result',
-        'result_mode',
+        '_background',
+        '_thunk',
+        '_rooeee',
+        '_bwop',
+        '_player_mon',
+        '_enemy_mon',
+        '_player_action',
+        '_enemy_action',
+        '_action_display',
+        '_action_set',
+        '_result_displayed',
+        '_result',
+        '_result_mode',
     )
 
     def __init__(self, player_mon: Monster, enemy_mon: Monster, draw_mode: Mode, win_mode: Mode, lose_mode: Mode):
         """The functions passed in should return the next mode."""
         super().__init__()
 
-        self.background = pygame.image.load(constants.LAYOUT_2_FILE)
+        self._background = pygame.image.load(constants.LAYOUT_2_FILE)
         for index, choice in enumerate(self._BOX_CHOICES):
             shared.font_wrap.renderToInside(
-                self.background,
+                self._background,
                 self.textStart(index),
                 self.textWidth(index),
                 choice,
                 False,
                 constants.TEXT_COLOR
             )
-        self.background = self.background.convert(shared.display.screen)
-        self.background.set_colorkey(constants.COLORKEY)
+        self._background = self._background.convert(shared.display.screen)
+        self._background.set_colorkey(constants.COLORKEY)
 
         pygame.mixer.music.load(constants.FIGHT_LOOP)
         pygame.mixer.music.play(-1)
 
-        self.thunk = pygame.mixer.Sound(constants.THUNK)
-        self.rooeee = pygame.mixer.Sound(constants.ROOEEE)
-        self.bwop = pygame.mixer.Sound(constants.BWOP)
+        self._thunk = pygame.mixer.Sound(constants.THUNK)
+        self._rooeee = pygame.mixer.Sound(constants.ROOEEE)
+        self._bwop = pygame.mixer.Sound(constants.BWOP)
 
-        self.player_mon = player_mon
-        self.enemy_mon = enemy_mon
+        self._player_mon = player_mon
+        self._enemy_mon = enemy_mon
 
-        self.player_mon.fightStart()
-        self.player_mon.setImage(True)
+        self._player_mon.fightStart()
+        self._player_mon.setImage(True)
 
-        self.player_mon.rect.midbottom = self._PLAYER_POS
-        self.enemy_mon.rect.midbottom = self._ENEMY_POS
-        self.all_sprites.add(self.player_mon, self.enemy_mon)
+        self._player_mon.rect.midbottom = self._PLAYER_POS
+        self._enemy_mon.rect.midbottom = self._ENEMY_POS
+        self.all_sprites.add(self._player_mon, self._enemy_mon)
 
-        self.player_action = False
-        self.enemy_action = False
+        self._player_action = False
+        self._enemy_action = False
 
-        self.action_display = deque((), 4)
-        self.action_set = False
+        self._action_display = deque((), 4)
+        self._action_set = False
 
-        self.result_displayed = 0
+        self._result_displayed = 0
 
-        self.result = False
-        self.result_mode = {
+        self._result = False
+        self._result_mode = {
             'draw': draw_mode,
             'win': win_mode,
             'lose': lose_mode,
         }
 
     def _buttonPress(self):
-        print(self.player_mon.rect.midbottom)
-        self.player_action = self._BOX_CHOICES[self._selected_button]
-        self.enemy_action = random.choice(('Attack', 'Defend'))
+        print(self._player_mon.rect.midbottom)
+        self._player_action = self._BOX_CHOICES[self._selected_button]
+        self._enemy_action = random.choice(('Attack', 'Defend'))
 
-        if self.player_action == 'Attack':
+        if self._player_action == 'Attack':
             self._setActionDisplay("I'm gonna hit 'em!")
-            self.player_mon.addWait(self._ANIM_WAIT)
-            self.player_mon.addPosRel(AnimSprite.Lerp, 200, 12, 0, sound=self.thunk)
-            self.player_mon.addPosRel(AnimSprite.Lerp, 200, -12, 0)
-        elif self.player_action == 'Defend':
+            self._player_mon.addWait(self._ANIM_WAIT)
+            self._player_mon.addPosRel(AnimSprite.Lerp, 200, 12, 0, sound=self._thunk)
+            self._player_mon.addPosRel(AnimSprite.Lerp, 200, -12, 0)
+        elif self._player_action == 'Defend':
             self._setActionDisplay("I'm gonna block 'em!")
-            self.player_mon.addWait(self._ANIM_WAIT)
-            self.player_mon.addPosRel(AnimSprite.Lerp, 133, -8, 0, sound=self.bwop)
-            self.player_mon.addPosRel(AnimSprite.Lerp, 200, 12, 0)
-            self.player_mon.addPosRel(AnimSprite.Lerp, 67, -4, 0)
-        elif self.player_action == 'Escape':
+            self._player_mon.addWait(self._ANIM_WAIT)
+            self._player_mon.addPosRel(AnimSprite.Lerp, 133, -8, 0, sound=self._bwop)
+            self._player_mon.addPosRel(AnimSprite.Lerp, 200, 12, 0)
+            self._player_mon.addPosRel(AnimSprite.Lerp, 67, -4, 0)
+        elif self._player_action == 'Escape':
             self._setActionDisplay("I'm gonna run away!")
-            self.player_mon.addWait(self._ANIM_WAIT)
-            self.player_mon.addWait(0, sound=self.rooeee)
-            self.player_mon.addPosRel(AnimSprite.Lerp, 333, -20, 0)
-            self.player_mon.addPosRel(AnimSprite.Lerp, 67, 20, 0)
+            self._player_mon.addWait(self._ANIM_WAIT)
+            self._player_mon.addWait(0, sound=self._rooeee)
+            self._player_mon.addPosRel(AnimSprite.Lerp, 333, -20, 0)
+            self._player_mon.addPosRel(AnimSprite.Lerp, 67, 20, 0)
 
-        if self.enemy_action == 'Attack':
-            self.enemy_mon.addWait(self._ANIM_WAIT)
-            self.enemy_mon.addPosRel(AnimSprite.Lerp, 200, -12, 0, sound=self.thunk)
-            self.enemy_mon.addPosRel(AnimSprite.Lerp, 200, 12, 0)
+        if self._enemy_action == 'Attack':
+            self._enemy_mon.addWait(self._ANIM_WAIT)
+            self._enemy_mon.addPosRel(AnimSprite.Lerp, 200, -12, 0, sound=self._thunk)
+            self._enemy_mon.addPosRel(AnimSprite.Lerp, 200, 12, 0)
             pass
-        elif self.enemy_action == 'Defend':
-            self.enemy_mon.addWait(self._ANIM_WAIT)
-            self.enemy_mon.addPosRel(AnimSprite.Lerp, 133, 8, 0, sound=self.bwop)
-            self.enemy_mon.addPosRel(AnimSprite.Lerp, 200, -12, 0)
-            self.enemy_mon.addPosRel(AnimSprite.Lerp, 67, 4, 0)
+        elif self._enemy_action == 'Defend':
+            self._enemy_mon.addWait(self._ANIM_WAIT)
+            self._enemy_mon.addPosRel(AnimSprite.Lerp, 133, 8, 0, sound=self._bwop)
+            self._enemy_mon.addPosRel(AnimSprite.Lerp, 200, -12, 0)
+            self._enemy_mon.addPosRel(AnimSprite.Lerp, 67, 4, 0)
             pass
 
     def _input(self, event):
         # click forward to next mode
-        if self.result:
+        if self._result:
             if (event.type == pygame.MOUSEBUTTONUP and event.button == 1) \
                     or (event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN):
                 self._stopMixer()
-                self.next_mode = self.result_mode[self.result]()
+                self.next_mode = self._result_mode[self._result]()
                 return
         # in the middle of action display
-        if self.player_action:
+        if self._player_action:
             return
         if event.type == pygame.MOUSEMOTION:
             self.posSelect(event.pos)
@@ -168,18 +168,18 @@ class ModeFight(ModeButtons):
                 self._buttonPress()
             # testing stuff, remove later
             elif event.key == pygame.K_t:
-                print("player_mon.stats = " + str(self.player_mon.stats))
-                print("enemy_mon.stats = " + str(self.enemy_mon.stats))
+                print("_player_mon.stats = " + str(self._player_mon.stats))
+                print("_enemy_mon.stats = " + str(self._enemy_mon.stats))
             else:
                 self.keySelect(event.key)
 
     def _setActionDisplay(self, text: str):
-        self.action_display.appendleft(shared.font_wrap.renderInside(200, text, False, constants.TEXT_COLOR))
-        self.action_set = not self.action_set
+        self._action_display.appendleft(shared.font_wrap.renderInside(200, text, False, constants.TEXT_COLOR))
+        self._action_set = not self._action_set
 
     def _playerActionDone(self):
-        player_attack_defend = self.player_mon.fightHit(self.player_action)
-        enemy_attack_defend = self.enemy_mon.fightHit(self.enemy_action)
+        player_attack_defend = self._player_mon.fightHit(self._player_action)
+        enemy_attack_defend = self._enemy_mon.fightHit(self._enemy_action)
         # print "player_attack_defend = " + str(player_attack_defend)
         # print "enemy_attack_defend  = " + str(enemy_attack_defend)
         damage_to_player = utility.reduceNumber(
@@ -200,60 +200,60 @@ class ModeFight(ModeButtons):
             damage_to_player = damage_to_enemy = 1
         # display results below
         self._setActionDisplay("Hit for " + str(damage_to_enemy) + "! Took " + str(damage_to_player) + "!")
-        self.player_mon.stats['hpc'] -= damage_to_player
-        self.enemy_mon.stats['hpc'] -= damage_to_enemy
+        self._player_mon.stats['hpc'] -= damage_to_player
+        self._enemy_mon.stats['hpc'] -= damage_to_enemy
 
-        if self.player_mon.stats['hpc'] < 1 and self.enemy_mon.stats['hpc'] < 1:
+        if self._player_mon.stats['hpc'] < 1 and self._enemy_mon.stats['hpc'] < 1:
             self._setupEnd('draw')
-        elif self.enemy_mon.stats['hpc'] < 1:
+        elif self._enemy_mon.stats['hpc'] < 1:
             self._setupEnd('win')
-        elif self.player_mon.stats['hpc'] < 1:
+        elif self._player_mon.stats['hpc'] < 1:
             self._setupEnd('lose')
         else:
-            self.player_action = False
-        self.enemy_action = False
+            self._player_action = False
+        self._enemy_action = False
 
     def _setupEnd(self, ending: str):
-        self.player_action = ending
-        self.player_mon.addWait(500)
-        self.player_mon.addWait(500)
+        self._player_action = ending
+        self._player_mon.addWait(500)
+        self._player_mon.addWait(500)
         pygame.mixer.music.fadeout(1000)
 
     def _update(self, dt):
-        if self.player_action in self._BOX_CHOICES and not self.player_mon.stillAnimating():
+        if self._player_action in self._BOX_CHOICES and not self._player_mon.stillAnimating():
             self._playerActionDone()
-        elif self.player_action == 'draw':
+        elif self._player_action == 'draw':
             self._endStuff("They're both out cold.")
-        elif self.player_action == 'win':
+        elif self._player_action == 'win':
             self._endStuff("I won!!!")
-        elif self.player_action == 'lose':
-            self._endStuff(self.player_mon.name + "'s out cold.")
+        elif self._player_action == 'lose':
+            self._endStuff(self._player_mon.name + "'s out cold.")
 
     def _endStuff(self, result_display: str):
-        if len(self.player_mon.anims) < 2 and self.result_displayed < 1:
+        if len(self._player_mon.anims) < 2 and self._result_displayed < 1:
             self._setActionDisplay(result_display)
-            self.result_displayed = 1
-        elif not self.player_mon.stillAnimating() and self.result_displayed < 2:
+            self._result_displayed = 1
+        elif not self._player_mon.stillAnimating() and self._result_displayed < 2:
             self._setActionDisplay("Click or press enter to")
             self._setActionDisplay("continue.")
-            self.result_displayed = 2
-            self.result = self.player_action
+            self._result_displayed = 2
+            self._result = self._player_action
 
     def _drawScreen(self, screen):
         screen.fill(constants.WHITE)
-        screen.blit(self.background, (0, 0))
-        if not self.action_set and self.player_action not in self.result_mode:
+        screen.blit(self._background, (0, 0))
+        if not self._action_set and self._player_action not in self._result_mode:
             self.drawSelected(screen)
 
         player_bar_length = self._HEALTH_BAR_LENGTH \
-            * self.player_mon.stats['hpc'] // self.player_mon.stats['hpm']
-        screen.fill(self.player_mon.getLightSkin(), (138, 30, player_bar_length, 10))
+            * self._player_mon.stats['hpc'] // self._player_mon.stats['hpm']
+        screen.fill(self._player_mon.getLightSkin(), (138, 30, player_bar_length, 10))
         screen.blit(self._health_bar, (137, 29))
 
         enemy_bar_length = self._HEALTH_BAR_LENGTH \
-            * self.enemy_mon.stats['hpc'] // self.enemy_mon.stats['hpm']
-        screen.fill(self.enemy_mon.getLightSkin(), (294 - enemy_bar_length, 30, enemy_bar_length, 10))
+            * self._enemy_mon.stats['hpc'] // self._enemy_mon.stats['hpm']
+        screen.fill(self._enemy_mon.getLightSkin(), (294 - enemy_bar_length, 30, enemy_bar_length, 10))
         screen.blit(self._health_bar, (233, 29))
         # maybe draw health numbers / stats / etc
-        for index, line in enumerate(self.action_display):
+        for index, line in enumerate(self._action_display):
             screen.blit(line, (120, 166 - constants.FONT_HEIGHT * index))

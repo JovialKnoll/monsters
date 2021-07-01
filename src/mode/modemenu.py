c697320c5f0a20ed27e7e481bff48ccb17bcf691
JovialKnoll <BrettCKaplan@gmail.com>
2021-06-30 22:04:53 -0400

    to fighting!

--- src/mode/modemenu.py
+++ src/mode/modemenu.py
@@ -1,10 +1,18 @@
 import math
 
 import shared
+from monster import Monster
+
+from personality import Personality
 from .modeconvo import ModeConvo
+from .modefight import ModeFight
 
 
 class ModeMenu(ModeConvo):
+    @staticmethod
+    def _getSpacing(stat_num: int):
+        return (2 - math.ceil(math.log10(stat_num))) * "_"
+
     def _handleLoad(self):
         if self._convo_key == "0":
             mon = shared.state.protag_mon
@@ -15,7 +23,27 @@ class ModeMenu(ModeConvo):
             mon_string += "\ndrv: " + str(mon.stats['drv']) + self._getSpacing(mon.stats['drv'])
             mon_string += "__hp: " + str(mon.stats['hpc']) + "/" + str(mon.stats['hpm'])
             self._text = mon.name + "\n" + mon_string.upper()
+        elif self._convo_key == "3a3":
+            if shared.state.protag_mon.personality == Personality.Affectionate:
+                self._text = "I'm sure we'll do great."
+            elif shared.state.protag_mon.personality == Personality.Aggressive:
+                self._text = "Let's go win a fight!"
+            elif shared.state.protag_mon.personality == Personality.Careful:
+                self._text = "Be careful, okay?"
+            elif shared.state.protag_mon.personality == Personality.Energetic:
+                self._text = "I'm so excited!"
 
-    @staticmethod
-    def _getSpacing(stat_num: int):
-        return (2 - math.ceil(math.log10(stat_num))) * "_"
+    def _handleButton(self, prev_convo_key: str, index: int):
+        if index == "3a3":
+            self._stopMixer()
+            self.next_mode = ModeFight(
+                shared.state.protag_mon,
+                Monster.atLevel(0),
+                ModeMonConvoTest,
+                ModeMonConvoTest,
+                ModeTest
+            )
+            # rewrite ModeFight:
+            # instead of switching based on outcome, write outcome to shared (and just go to a passed in mode)
+            return True
+        return False

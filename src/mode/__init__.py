938455a6571eeaa74d0b4536dbcf4f1ad525cf26
JovialKnoll <BrettCKaplan@gmail.com>
2021-09-05 17:38:10 -0400

    empty next mode for level up and text for leadup to that

--- src/mode/__init__.py
+++ src/mode/__init__.py
@@ -6,6 +6,7 @@ from .modegamemenu import ModeGameMenu
 from .modegamemenu import ModeGameMenuTop
 from .modeintroduction0 import ModeIntroduction0
 from .modeintroduction1 import ModeIntroduction1
+from .modelevelup0 import ModeLevelUp0
 from .modemenu import ModeMenu
 from .modemonconvotest import ModeMonConvoTest
 from .modeopening import ModeOpening

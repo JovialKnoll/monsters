db49e2da31599b0b683df56e4d02d4b78732a68e
JovialKnoll <BrettCKaplan@gmail.com>
2021-12-06 22:22:23 -0500

    set up next menu mode

--- src/mode/modemenu1.py
+++ src/mode/modemenu1.py
@@ -2,31 +2,40 @@ import shared
 from monster import Monster
 from personality import Personality
 from .modefight import ModeFight
-# from .modetalkwin1 import ModeTalkWin1
-# from .modetalkelse1 import ModeTalkElse1
+from .modetalkwin1 import ModeTalkWin1
+from .modetalkelse1 import ModeTalkElse1
 from .modemenu import ModeMenu
 
 
 class ModeMenu1(ModeMenu):
     def _handleLoad(self):
         super()._handleLoad()
-        # if self._convo_key == "3a3":
-        #     if shared.state.protag_mon.personality == Personality.Affectionate:
-        #         self._text = "I'm sure we'll do great."
-        #     elif shared.state.protag_mon.personality == Personality.Aggressive:
-        #         self._text = "Let's go win a fight!"
-        #     elif shared.state.protag_mon.personality == Personality.Careful:
-        #         self._text = "Be careful, okay?"
-        #     elif shared.state.protag_mon.personality == Personality.Energetic:
-        #         self._text = "I'm so excited!"
+        if self._convo_key == "1":
+            if shared.state.protag_mon.personality == Personality.Affectionate:
+                self._text = "Thanks for helping me win that fight!"
+            elif shared.state.protag_mon.personality == Personality.Aggressive:
+                self._text = "Okay, we need to go do that again!"
+            elif shared.state.protag_mon.personality == Personality.Careful:
+                self._text = "That was a little scary, but so cool!"
+            elif shared.state.protag_mon.personality == Personality.Energetic:
+                self._text = "That was exciting!"
+        elif self._convo_key == "3a1":
+            if shared.state.protag_mon.personality == Personality.Affectionate:
+                self._text = "Thanks for helping me!"
+            elif shared.state.protag_mon.personality == Personality.Aggressive:
+                self._text = "We'll get them good!"
+            elif shared.state.protag_mon.personality == Personality.Careful:
+                self._text = "Help me out again, okay?"
+            elif shared.state.protag_mon.personality == Personality.Energetic:
+                self._text = "We've got this!"
 
     def _handleButton(self, prev_convo_key, index):
-        # if prev_convo_key == "3a3":
-        #     self._stopMixer()
-        #     self.next_mode = ModeFight(
-        #         shared.state.protag_mon,
-        #         Monster.atLevel(1),
-        #         lambda: ModeTalkWin1() if shared.state.fight_results[-1] == 1 else ModeTalkElse1()
-        #     )
-        #     return True
+        if prev_convo_key == "3a1":
+            self._stopMixer()
+            self.next_mode = ModeFight(
+                shared.state.protag_mon,
+                Monster.atLevel(1),
+                lambda: ModeTalkWin1() if shared.state.fight_results[-1] == 1 else ModeTalkElse1()
+            )
+            return True
         return False

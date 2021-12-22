a3d42981a1fc81e6c302220c5d46072c04346db4
Brett Kaplan <BrettCKaplan@gmail.com>
2021-12-21 22:53:08 -0500

    wrong level

--- src/mode/modetalkelse1.py
+++ src/mode/modetalkelse1.py
@@ -11,7 +11,7 @@ class ModeTalkElse1(ModeConvo):
             self._stopMixer()
             self.next_mode = ModeFight(
                 shared.state.protag_mon,
-                Monster.atLevel(0),
+                Monster.atLevel(1),
                 lambda: ModeTalkWin1() if shared.state.fight_results[-1] == 1 else ModeTalkElse1()
             )
             return True

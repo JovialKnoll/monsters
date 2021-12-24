274af1f8af7f639d0f8ab26e96bd93516cdf8e36
Brett Kaplan <BrettCKaplan@gmail.com>
2021-12-24 18:29:31 -0500

    mode menu 2 setup

--- src/mode/modemenu2.py
+++ src/mode/modemenu2.py
@@ -10,27 +10,18 @@ from .modemenu import ModeMenu
 class ModeMenu2(ModeMenu):
     def _handleLoad(self):
         super()._handleLoad()
-        # if self._convo_key == "1":
-        #     if shared.state.protag_mon.personality == Personality.Affectionate:
-        #         self._text = "Thanks for helping me win that fight!"
-        #     elif shared.state.protag_mon.personality == Personality.Aggressive:
-        #         self._text = "Okay, we need to go do that again!"
-        #     elif shared.state.protag_mon.personality == Personality.Careful:
-        #         self._text = "That was a little scary, but so cool!"
-        #     elif shared.state.protag_mon.personality == Personality.Energetic:
-        #         self._text = "That was exciting!"
-        # elif self._convo_key == "3a1":
-        #     if shared.state.protag_mon.personality == Personality.Affectionate:
-        #         self._text = "Thanks for helping me!"
-        #     elif shared.state.protag_mon.personality == Personality.Aggressive:
-        #         self._text = "We'll get them good!"
-        #     elif shared.state.protag_mon.personality == Personality.Careful:
-        #         self._text = "Help me out again, okay?"
-        #     elif shared.state.protag_mon.personality == Personality.Energetic:
-        #         self._text = "We've got this!"
+        if self._convo_key == "2":
+            if shared.state.protag_mon.personality == Personality.Affectionate:
+                self._text = "Happy and healthy."
+            elif shared.state.protag_mon.personality == Personality.Aggressive:
+                self._text = "Ready to win again."
+            elif shared.state.protag_mon.personality == Personality.Careful:
+                self._text = "Probably ready to go."
+            elif shared.state.protag_mon.personality == Personality.Energetic:
+                self._text = "Excited and ready."
 
     def _handleButton(self, prev_convo_key, index):
-        if prev_convo_key == "xxx":
+        if prev_convo_key == "3a":
             self._stopMixer()
             self.next_mode = ModeFight(
                 shared.state.protag_mon,

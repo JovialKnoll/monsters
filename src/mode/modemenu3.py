96a2c511e17a7cdac490935d56dfc1317cee3ef8
Brett Kaplan <BrettCKaplan@gmail.com>
2021-12-30 17:07:28 -0500

    menu 3 class

--- src/mode/modemenu3.py
+++ src/mode/modemenu3.py
@@ -8,29 +8,8 @@ from .modemenu import ModeMenu
 
 
 class ModeMenu3(ModeMenu):
-    def _handleLoad(self):
-        super()._handleLoad()
-        # if self._convo_key == "1":
-        #     if shared.state.protag_mon.personality == Personality.Affectionate:
-        #         self._text = "Thanks for helping me win that fight!"
-        #     elif shared.state.protag_mon.personality == Personality.Aggressive:
-        #         self._text = "Okay, we need to go do that again!"
-        #     elif shared.state.protag_mon.personality == Personality.Careful:
-        #         self._text = "That was a little scary, but so cool!"
-        #     elif shared.state.protag_mon.personality == Personality.Energetic:
-        #         self._text = "That was exciting!"
-        # elif self._convo_key == "3a1":
-        #     if shared.state.protag_mon.personality == Personality.Affectionate:
-        #         self._text = "Thanks for helping me!"
-        #     elif shared.state.protag_mon.personality == Personality.Aggressive:
-        #         self._text = "We'll get them good!"
-        #     elif shared.state.protag_mon.personality == Personality.Careful:
-        #         self._text = "Help me out again, okay?"
-        #     elif shared.state.protag_mon.personality == Personality.Energetic:
-        #         self._text = "We've got this!"
-
     def _handleButton(self, prev_convo_key, index):
-        if prev_convo_key == "xxx":
+        if prev_convo_key == "3aa":
             self._stopMixer()
             self.next_mode = ModeFight(
                 shared.state.protag_mon,

61547f861f9e0a0d7d372c1d9a3257255c475aa0
JovialKnoll <BrettCKaplan@gmail.com>
2022-01-21 22:34:32 -0500

    cleanup

--- src/game.py
+++ src/game.py
@@ -71,7 +71,7 @@ class Game(object):
         return True
 
     def _handlePauseMenu(self):
-        # pass quit events forward to the mode.ModeGameMenu, but not to other events
+        # pass quit events forward to mode.ModeGameMenu, but not to other events
         if isinstance(self._current_mode, mode.ModeGameMenu):
             return True
         self._current_mode = mode.ModeGameMenuTop(self._current_mode)

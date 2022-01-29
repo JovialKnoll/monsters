548c4fe7435f1e7895bcadf3e61ad88cdae653b9
JovialKnoll <BrettCKaplan@gmail.com>
2022-01-29 14:33:15 -0500

    not one file

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -6,12 +6,11 @@ src/venv/Scripts/activate.ps1
 # get pyinstaller
 pip install pyinstaller
 # create spec file
-pyinstaller --clean -ywF -i design/icon.ico -n chikkai src/main.py
+pyinstaller --clean -yw -i design/icon.ico -n chikkai src/main.py
 # build exe
 pyinstaller -y chikkai.spec
 # copy in assets
 cp -r src/assets dist/chikkai/assets
-mv dist/chikkai.exe dist/chikkai/
 # cleanup
 deactivate
 rm -r build

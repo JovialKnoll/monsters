998b0f3886fc8e067f29d8daf1791ca825a7f938
JovialKnoll <BrettCKaplan@gmail.com>
2022-01-30 12:10:01 -0500

    reorder

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -14,10 +14,10 @@ mkdir dist/chikkai
 mv dist/chikkai.exe dist/chikkai/chikkai.exe
 # copy in assets
 cp -r src/assets dist/chikkai/assets
-# copy in readme
-cp README.txt dist/chikkai/
 # copy in license
 cp LICENSE.txt dist/chikkai/
+# copy in readme
+cp README.txt dist/chikkai/
 # cleanup
 deactivate
 rm -r build

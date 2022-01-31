9d8c9662ca708fd886d9b8db30ff2be474e778bd
JovialKnoll <BrettCKaplan@gmail.com>
2022-01-30 21:02:01 -0500

    finishing up credits mode

--- src/mode/modecredits.py
+++ src/mode/modecredits.py
@@ -1,3 +1,5 @@
+import pygame
+
 import constants
 import shared
 from animsprite import AnimSprite
@@ -7,47 +9,54 @@ from .modeopening import ModeOpening
 
 class ModeCredits(ModeOpening):
     __slots__ = (
-        '_time',
+        '_background',
+        '_credits_sprite',
     )
 
     def __init__(self):
         super().__init__()
-        self._time = 0
-        # could replace time tracking with tracking position of sprite
+        self._background = pygame.Surface(constants.SCREEN_SIZE).convert(shared.display.screen)
+        self._background.fill(constants.BLACK)
         with open(constants.CREDITS_TEXT) as credits_file:
             credits_text = credits_file.read().replace(' ', '_')
-        credits_sprite = AnimSprite()
-        credits_sprite.image = shared.font_wrap.renderInside(
+        self._credits_sprite = AnimSprite()
+        self._credits_sprite.image = shared.font_wrap.renderInside(
             constants.SCREEN_SIZE[0] // 2,
             credits_text,
             False,
             constants.WHITE,
             background=constants.BLACK
         )
-        credits_sprite.rect = credits_sprite.image.get_rect()
-        credits_sprite.rect.midtop = (
+        self._credits_sprite.rect = self._credits_sprite.image.get_rect()
+        self._credits_sprite.rect.midtop = (
             constants.SCREEN_SIZE[0] // 2,
             constants.SCREEN_SIZE[1],
         )
-        credits_sprite.addWait(1000)
+        self._credits_sprite.addWait(1000)
         credits_speed = constants.FONT_HEIGHT / 750
-        credits_distance = constants.SCREEN_SIZE[1] + credits_sprite.rect.height
+        credits_distance = constants.SCREEN_SIZE[1] + self._credits_sprite.rect.height
         credits_time = int(credits_distance / credits_speed)
-        credits_sprite.addPosRel(
+        self._credits_sprite.addPosRel(
             AnimSprite.Lerp,
             credits_time,
             0,
             credits_distance * -1
         )
-        self.all_sprites.add(credits_sprite)
+        self._credits_sprite.addWait(1000)
+        self.all_sprites.add(self._credits_sprite)
 
     def _switchMode(self):
         self.next_mode = ModeOpening0()
 
     def _update(self, dt):
-        self._time += dt
-        # after credits_time, render text on to background
-        # press any key to proceed etc
+        if not self._credits_sprite.stillAnimating():
+            shared.font_wrap.renderToCentered(
+                self._background,
+                (constants.SCREEN_SIZE[0] // 2, constants.SCREEN_SIZE[1] // 2 + 4),
+                "press any key to proceed",
+                False,
+                constants.WHITE
+            )
 
     def _drawScreen(self, screen):
-        screen.fill(constants.BLACK)
+        screen.blit(self._background, (0, 0))

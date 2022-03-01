e7a4d7af7ded0ed29f22196f142736ab4d72da39
JovialKnoll <BrettCKaplan@gmail.com>
2022-02-28 20:42:23 -0500

    rearranging needed classes in file

--- src/mode/modeconvo.py
+++ src/mode/modeconvo.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import copy
 
@@ -6,9 +7,68 @@ import pygame
 import constants
 import utility
 import shared
-from convopart import ConvoPart
 from saveable import Saveable
 from .modebuttons import ModeButtons
+import mode
+
+
+class ConvoChoice(object):
+    __slots__ = (
+        'text',
+        'key',
+    )
+
+    def __init__(self, text: str, key: str):
+        self.text = text
+        self.key = key
+
+    def getNextMode(self):
+        if hasattr(mode, self.key):
+            mode_cls = getattr(mode, self.key)
+            # currently only handling modes that can be created with no arguments
+            return mode_cls
+        return None
+
+
+class ConvoPart(object):
+    __slots__ = (
+        'style',
+        'text',
+        'choices',
+    )
+
+    def __init__(self, style: set[str], text: str, choices: list[ConvoChoice]):
+        self.style = style
+        self.text = text
+        self.choices = choices
+
+    @staticmethod
+    def getConvoDict(convo_file: str):
+        convo_dict = {}
+        with open(convo_file) as convo_data:
+            convo_reader = csv.reader(convo_data)
+            for row in convo_reader:
+                row_iter = iter(row)
+                key = next(row_iter)
+                if key in convo_dict:
+                    raise ValueError(f"The convo file {convo_file} has a duplicate row key {key}.")
+                style = {tag.strip() for tag in next(row_iter).upper().split('|')}
+                text = next(row_iter)
+                choices = []
+                try:
+                    for i in range(len(ModeConvo.buttons)):
+                        choice_text = next(row_iter)
+                        choice_key = next(row_iter)
+                        if choice_key:
+                            choice = ConvoChoice(choice_text, choice_key)
+                            choices.append(choice)
+                except StopIteration:
+                    pass
+                if not choices:
+                    raise ValueError(f"The convo file {convo_file} has no choices in the row with key {key}.")
+                convo_part = ConvoPart(style, text, choices)
+                convo_dict[key] = convo_part
+        return convo_dict
 
 
 class ModeConvo(ModeButtons, Saveable):

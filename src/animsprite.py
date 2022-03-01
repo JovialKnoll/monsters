c144b1870e6e5921b60cff51b0ca01f99ac74d39
JovialKnoll <BrettCKaplan@gmail.com>
2022-02-28 21:06:05 -0500

    move Anim in with AnimSprite

--- src/animsprite.py
+++ src/animsprite.py
@@ -4,8 +4,35 @@ import pygame
 
 import constants
 import utility
-from anim import Anim
 from saveable import Saveable
+
+
+class Anim(Saveable):
+    __slots__ = (
+        'func',
+        'time',
+        'pos',
+        'sound',
+        'positional_sound',
+    )
+
+    def __init__(self, func: str, time: int, x_or_pair, y=None,
+                 sound: pygame.mixer.Sound = None, positional_sound: bool = False):
+        self.func = func
+        self.time = time
+        self.pos = pygame.math.Vector2(x_or_pair, y)
+        self.sound = sound
+        self.positional_sound = positional_sound
+
+    def save(self):
+        # no sound right now, sorry
+        # if we need it, either start passing sounds as paths
+        # or don't save when there are pending Anims
+        return self.func, self.time, self.pos
+
+    @classmethod
+    def load(cls, save_data):
+        return cls(*save_data)
 
 
 class AnimSprite(pygame.sprite.DirtySprite, Saveable):

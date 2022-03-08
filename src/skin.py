55e263a78c5d70869317455457df4f7cc2090f41
JovialKnoll <BrettCKaplan@gmail.com>
2022-03-07 19:18:14 -0500

    skintone can go in skin file

--- src/skin.py
+++ src/skin.py
@@ -1,8 +1,28 @@
 import random
 
-from skintone import SkinTone
+import pygame
+import jovialengine
+
 from personality import Personality
 
+
+class SkinTone(jovialengine.Saveable):
+    __slots__ = (
+        'dark',
+        'light',
+    )
+
+    def __init__(self, in_dark: tuple[int, int, int], in_light: tuple[int, int, int]):
+        """Hold onto the darker and lighter colors."""
+        self.dark = pygame.Color(in_dark[0], in_dark[1], in_dark[2])
+        self.light = pygame.Color(in_light[0], in_light[1], in_light[2])
+
+    def save(self):
+        return (self.dark.r, self.dark.g, self.dark.b), (self.light.r, self.light.g, self.light.b)
+
+    @classmethod
+    def load(cls, save_data):
+        return cls(*save_data)
 
 class Skin(object):
     start_tone = SkinTone((72, 79, 69), (123, 129, 121))

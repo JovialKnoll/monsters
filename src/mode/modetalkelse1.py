8688ff0d876da29256b608676a5797e17d559f91
JovialKnoll <BrettCKaplan@gmail.com>
2022-06-14 22:12:28 -0400

    new function name for getting game

--- src/mode/modetalkelse1.py
+++ src/mode/modetalkelse1.py
@@ -11,9 +11,9 @@ class ModeTalkElse1(ModeConvo):
         if prev_convo_key == "4a":
             self._stopMixer()
             self.next_mode = ModeFight(
-                jovialengine.game.getGame().state.protag_mon,
+                jovialengine.game.getInstance().state.protag_mon,
                 Monster.atLevel(1),
-                lambda: ModeTalkWin1() if jovialengine.game.getGame().state.fight_results[-1] == 1 else ModeTalkElse1()
+                lambda: ModeTalkWin1() if jovialengine.game.getInstance().state.fight_results[-1] == 1 else ModeTalkElse1()
             )
             return True
         return False

ad56bb9e8693804732a9d81b78e6df47b579fe44
JovialKnoll <BrettCKaplan@gmail.com>
2022-07-08 11:15:24 -0400

    clean up and spacing

--- src/main.py
+++ src/main.py
@@ -4,16 +4,17 @@ import sys
 import os
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"
-import pygame
-pygame.init()
-import jovialengine
-game = jovialengine.getGame()
-from mode import ModeOpening0
-game.load(ModeOpening0)
 
+import pygame
+import jovialengine
+
+game = jovialengine.getGame()
+
+from mode import ModeOpening0
+
+game.load(ModeOpening0)
 while game.run():
     pass
 
-del game
 pygame.quit()
 sys.exit()

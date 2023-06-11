32fd8e8320fcc6cdabbae96af154832149358bf7
Brett Kaplan <BrettCKaplan@gmail.com>
2023-06-11 14:58:45 -0400

    don't add newline

--- scripts/test_package_windows.ps1
+++ scripts/test_package_windows.ps1
@@ -1,7 +1,7 @@
 cd ../../jovialengine
-((Get-Content -path setup.cfg -Raw) -replace 'VERSION_REPLACE','999') | Set-Content -Path setup.cfg
+((Get-Content -path setup.cfg -Raw) -replace 'VERSION_REPLACE','999') | Set-Content -Path setup.cfg -NoNewline
 python -m build --sdist --wheel --outdir dist/ . | Out-Null
-((Get-Content -path setup.cfg -Raw) -replace '999','VERSION_REPLACE') | Set-Content -Path setup.cfg
+((Get-Content -path setup.cfg -Raw) -replace '999','VERSION_REPLACE') | Set-Content -Path setup.cfg -NoNewline
 cd ../monsters
 src/venv/Scripts/activate.ps1
 pip install --force-reinstall ../jovialengine/dist/jovialengine-999-py3-none-any.whl | Out-Null

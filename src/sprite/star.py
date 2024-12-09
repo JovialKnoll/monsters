e61a88fd86484b7472b1f9fa937ddebfd0b35604
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-08 22:48:38 -0500

    actually assigning these

--- src/sprite/star.py
+++ src/sprite/star.py
@@ -4,9 +4,9 @@ import jovialengine
 import constants
 
 
-class Star(jovialengine.GameSprite):
-    _IMAGE_LOCATION: constants.STAR
-    _ALPHA_OR_COLORKEY: constants.COLORKEY
+class Star(jovialengine.AnimSprite):
+    _IMAGE_LOCATION = constants.STAR
+    _ALPHA_OR_COLORKEY = constants.COLORKEY
 
     def __init__(self, pos: pygame.typing.Point = (0, 0)):
         print(f"Star {pos}")
